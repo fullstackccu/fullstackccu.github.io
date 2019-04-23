c09409eec6f5c6fc2e71d93226c25b5266f5e64f
jjean_tw <ccumouse@gmail.com>
2019-04-23 10:37:34 +0800

    12

---
+++ lectures/12/ex3/scripts/app.js
@@ -0,0 +1,18 @@
+class App {
+  constructor(presentContainer, titleContainer) {
+    this.presentContainer = presentContainer;
+    this.titleContainer = titleContainer;
+    
+    this.presents = [];
+    this._fillPresentContainer();
+  } 
+  
+  _fillPresentContainer() {
+    for (const source of PRESENT_SOURCES) {
+      const present = new Present(this.presentContainer, source);
+      this.presents.push(present);
+    }
+  }
+}
+
+
