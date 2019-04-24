eee5f1c9b5ba1525c25e8fdd2465bf1a10610e0c
jjean_tw <ccumouse@gmail.com>
2019-04-24 23:12:57 +0800

    lecture 13

---
+++ lectures/13/ex1/scripts/app.js
@@ -0,0 +1,26 @@
+class App {
+  constructor(presentContainer, titleContainer) {
+    this.presentContainer = presentContainer;
+    this.titleContainer = titleContainer;
+    
+    this.presents = [];
+    this._fillPresentContainer();
+    this.openedCount = 0;
+  } 
+  
+  _fillPresentContainer() {
+    for (const source of PRESENT_SOURCES) {
+      const present = new Present(this.presentContainer, source, this);
+      this.presents.push(present);
+    }
+  }
+  
+	onPresentOpened() {
+    this.openedCount++;
+    if (this.openedCount === this.presents.length) {
+      this.titleContainer.textContent = 'Enjoy your presents!';
+    }
+  } 
+}
+
+
